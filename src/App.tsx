c7e09daf7127b557c9222b75df560939dcd0d8c2
narcello <marcellovcs@gmail.com>
2024-08-31 07:39:32 -0300

    feat: table pagination integrated with hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import useData from "./hooks/useData";
-import { DataTable } from "primereact/datatable";
+import { DataTable, DataTableStateEvent } from "primereact/datatable";
 import { Column } from "primereact/column";
 
 function App() {
@@ -10,28 +10,30 @@ function App() {
     getData();
   }, []);
 
+  const handlePagination = (statePagination: DataTableStateEvent) => {
+    getData({ page: statePagination.page! + 1 });
+  };
+
   return (
-    <>
-      <h3>{isLoading ? "Loading" : "Done"}</h3>
-      <h3>Pagination</h3>
-      <center>{JSON.stringify(pagination)}</center>
-      <br />
-      <DataTable
-        lazy
-        value={data}
-        rows={pagination?.limit}
-        paginator
-        totalRecords={pagination?.total}
-        tableStyle={{ minWidth: "50rem" }}
-      >
-        <Column field="title" header="Title"></Column>
-        <Column field="place_of_origin" header="Origin"></Column>
-        <Column field="artist_display" header="Artist"></Column>
-        <Column field="inscriptions" header="Inscriptions"></Column>
-        <Column field="date_start" header="Start Date"></Column>
-        <Column field="date_end" header="End Date"></Column>
-      </DataTable>
-    </>
+    <DataTable
+      lazy
+      paginator
+      stripedRows
+      value={data}
+      loading={isLoading}
+      rows={pagination?.limit}
+      totalRecords={pagination?.total}
+      first={pagination?.offset}
+      onPage={handlePagination}
+      size="small"
+    >
+      <Column field="title" header="Title"></Column>
+      <Column field="place_of_origin" header="Origin"></Column>
+      <Column field="artist_display" header="Artist"></Column>
+      <Column field="inscriptions" header="Inscriptions"></Column>
+      <Column field="date_start" header="Start Date"></Column>
+      <Column field="date_end" header="End Date"></Column>
+    </DataTable>
   );
 }
 
